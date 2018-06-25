a90750d20a6981fe57f82db2407eb60b7afedff0
Ed Hill <hill.charles2@gmail.com>
2018-06-25 14:28:04 -0400

    Updated acquisitions module to not use the dumn JSONDict class

--- oclc_wrappers/acquisitions.py
+++ oclc_wrappers/acquisitions.py
@@ -2,57 +2,61 @@ import json
 
 from oclc_wrappers.oclc_exceptions import RequestError
 from oclc_wrappers.requestor import HMACRequest
-from oclc_wrappers.constants import (PO_TEMPLATE, ITEM_TEMPLATE, ITEM_FUND_FIELDS, PO_URLS,
-                       ITEM_URLS, FUND_URLS)
-from oclc_wrappers.jsondict import JSONDict
+from oclc_wrappers.constants import PO_TEMPLATE, ITEM_TEMPLATE, ITEM_FUND_FIELDS, PO_URLS, ITEM_URLS, FUND_URLS
 
 
-class PurchaseOrder(JSONDict):
+class PurchaseOrder(object):
 
     def __init__(self, auth, *args, **kwargs):
-        self.update(PO_TEMPLATE)
-        super(PurchaseOrder, self).__init__(*args, **kwargs)
+        self._data = PO_TEMPLATE
+        self._data.update(*args, **kwargs)
         self.auth = auth
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
 
     @property
     def vendor_id(self):
-        return self['vendor']['vendorId']
+        return self._data['vendor']['vendorId']
 
     @vendor_id.setter
     def vendor_id(self, val):
-        self['vendor']['vendorId'] = val
+        self._data['vendor']['vendorId'] = val
 
     @property
     def vendor_local_id(self):
-        return self['vendor']['localIdentifier']
+        return self._data['vendor']['localIdentifier']
 
     @vendor_local_id.setter
     def vendor_local_id(self, val):
-        self['vendor']['localIdentifier'] = val
+        self._data['vendor']['localIdentifier'] = val
 
     @property
     def name(self):
-        return self['orderName']
+        return self._data['orderName']
 
     @name.setter
     def name(self, val):
-        self['orderName'] = val
+        self._data['orderName'] = val
 
     @property
     def number(self):
-        return self['purchaseOrderNumber']
+        return self._data['purchaseOrderNumber']
 
     @property
     def po_items(self):
-        return get_all_purchase_order_items(self.auth, self['purchaseOrderNumber'])
+        return get_all_purchase_order_items(self.auth, self._data['purchaseOrderNumber'])
 
     def create_in_wms(self):
-        r = send_purchase_order(self.auth, self)
-        self.clear()
-        self.update(json.loads(r.content))
+        r = send_purchase_order(self.auth, self._data)
+        self._data.clear()
+        self._data.update(json.loads(r.content))
 
 
-class Item(JSONDict):
+class Item(object):
     """
     Represent a purchase order item as a Python object.
 
@@ -63,30 +67,36 @@ class Item(JSONDict):
     The following fields are more or less required of an item to be able to
     send it to WMS:
     Resource:
-        self.oclc_number = self['resource']['worldcatResource']['oclcNumber']
+        self.oclc_number = self._data['resource']['worldcatResource']['oclcNumber']
     Fund:
-        self.first_vendor_id = self['copyConfigs']['copyConfig'][0]['booking'][0]['vendorId']
-        self.first_percentage = self['copyConfigs']['copyConfig'][0]['booking'][0]['percentage']
+        self.first_vendor_id = self._data['copyConfigs']['copyConfig'][0]['booking'][0]['vendorId']
+        self.first_percentage = self._data['copyConfigs']['copyConfig'][0]['booking'][0]['percentage']
     Branch:
-        self.first_copy_branch = self['copyConfigs']['copyConfig'][0]['branchId']
+        self.first_copy_branch = self._data['copyConfigs']['copyConfig'][0]['branchId']
     Shelving location:
-        self.first_copy_shelving = self['copyConfigs']['copyConfig'][0]['shelvingLocationId']
+        self.first_copy_shelving = self._data['copyConfigs']['copyConfig'][0]['shelvingLocationId']
     Order type:
-        self.order_type = self['orderType']
+        self.order_type = self._data['orderType']
     Price:
-        self.price = self['orderingPrice']
+        self.price = self._data['orderingPrice']
 
     :arg auth - An Auth object that implements HMAC authentication
     """
 
     def __init__(self, auth, *args, **kwargs):
-        self.update(ITEM_TEMPLATE)
-        super(Item, self).__init__(*args, **kwargs)
+        self._data = ITEM_TEMPLATE
+        self._data.update(*args, **kwargs)
         self.auth = auth
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
 
     @property
     def copies(self):
-        return self['copyConfigs']['copyConfig']
+        return self._data['copyConfigs']['copyConfig']
 
     @property
     def first_copy(self):
@@ -130,15 +140,15 @@ class Item(JSONDict):
 
     @property
     def order_type(self):
-        return self['orderType']
+        return self._data['orderType']
 
     @order_type.setter
     def order_type(self, val):
-        self['orderType'] = val
+        self._data['orderType'] = val
 
     @property
     def worldcat(self):
-        return self['resource']['worldcatResource']
+        return self._data['resource']['worldcatResource']
 
     @property
     def oclc_number(self):
@@ -150,11 +160,11 @@ class Item(JSONDict):
 
     @property
     def price(self):
-        return self['orderingPrice']
+        return self._data['orderingPrice']
 
     @price.setter
     def price(self, val):
-        self['orderingPrice'] = val
+        self._data['orderingPrice'] = val
 
     @property
     def title(self):
@@ -166,34 +176,34 @@ class Item(JSONDict):
 
     @property
     def vendor_item_number(self):
-        return self['vendorOrderItemNumber']
+        return self._data['vendorOrderItemNumber']
 
     @vendor_item_number.setter
     def vendor_item_number(self, val):
-        self['vendorOrderItemNumber'] = val
+        self._data['vendorOrderItemNumber'] = val
 
     @property
     def notes(self):
-        return ', '.join([x['content'] for x in self['notes']['note']])
+        return ', '.join([x['content'] for x in self._data['notes']['note']])
 
     @property
     def has_multiple_copies(self):
         return len(self.copies) > 1
 
     def attach_to_order(self, order):
-        new_item = attach_item_to_order(self.auth, order, self)
-        self.clear()
-        self.update(new_item)
+        new_item = attach_item_to_order(self.auth, order, self._data)
+        self._data.clear()
+        self._data.update(new_item)
 
     def add_isbn(self, isbn):
         self.worldcat['isbn'].append(isbn)
 
     def add_note(self, note):
-        self['notes']['note'].append({'content': note, 'type': 'STAFF', 'alert': 'NONE'})
+        self._data['notes']['note'].append({'content': note, 'type': 'STAFF', 'alert': 'NONE'})
 
     def add_notes(self, *args):
-        self['notes']['note'] = [{'content': note, 'type': 'STAFF', 'alert': 'NONE'}
-                                 for note in args if note]
+        self._data['notes']['note'] = [{'content': note, 'type': 'STAFF', 'alert': 'NONE'}
+                                       for note in args if note]
 
     def add_fund(self, copy=None, **kwargs):
         copy_index = self._get_copy_index(copy)
@@ -222,7 +232,7 @@ class Item(JSONDict):
         raise KeyError
 
     def add_copy(self, **kwargs):
-        self['copyConfigs']['copyConfig'].append(self._new_copy(**kwargs))
+        self._data['copyConfigs']['copyConfig'].append(self._new_copy(**kwargs))
         return len(self.copies) - 1
 
     def all_copies_same_branch(self, branch_id):
@@ -266,17 +276,29 @@ class Item(JSONDict):
             raise KeyError
 
 
-class Budget(JSONDict):
+class Budget(object):
 
     def __init__(self, *args, **kwargs):
-        super(Budget, self).__init__(*args, **kwargs)
+        self._data = dict(*args, **kwargs)
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
 
 
-class Fund(JSONDict):
+class Fund(object):
 
     def __init__(self, auth, *args, **kwargs):
-        super(Fund, self).__init__(*args, **kwargs)
+        self._data = dict(*args, **kwargs)
         self.auth = auth
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
 
     @property
     def allocation(self):
@@ -296,18 +318,18 @@ class Fund(JSONDict):
 
     @property
     def name(self):
-        return self['name']
+        return self._data['name']
 
     @property
     def code(self):
-        return self['code']
+        return self._data['code']
 
     @property
     def id(self):
-        return self['allocation']['allocation']['id']
+        return self._data['allocation']['allocation']['id']
 
     def _get_price(self, price_type):
-        return self['allocation']['allocation'][price_type]['priceSpecification']['price']
+        return self._data['allocation']['allocation'][price_type]['priceSpecification']['price']
 
 
 def po_request(auth):
